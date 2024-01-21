3d9a4e3cbc40be401341dfb6ae884b20d6cde632
Dhanendra Rahangdale <drrahangdale01@gmail.com>
2024-01-21 19:05:46 +0530

    Update user details  Functionality added  in app
    when user click on the specipic users edite button  so  user can change the vLUES  OF DETAILS

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
+///  when the page relode so all the previous user data will rendered on the screen
 
-
-
-window.addEventListener('DOMContentLoaded',(event)=>{
-    axios.get("https://crudcrud.com/api/3f4210f7265e4b6bb5c86a3348c859b1/BookingAppointment")
+window.addEventListener('DOMContentLoaded', (event) => {
+    getRequest()
         .then(response => {
-            for(let i = 0;i<response.data.length;i++){
-                showuserOnScreen(response.data[i],response.data[i]._id)
+            for (let i = 0; i < response.data.length; i++) {
+                showuserOnScreen(response.data[i], response.data[i]._id)
             }
         })
         .catch(error => console.log(error));
 })
+
+
 
 const form = document.getElementById('userDetailsForm');
 
@@ -25,46 +26,90 @@ form.addEventListener('submit', (event) => {
         phoneNumber
     }
 
-   
-    axios.post("https://crudcrud.com/api/3f4210f7265e4b6bb5c86a3348c859b1/BookingAppointment", obj)
-        .then(response => {showuserOnScreen(obj,response.data._id)})
-        .catch(error => console.log(error));
-  
+   ///  post request
+    postRequest(obj)
+    .then(response => { showuserOnScreen(obj, response.data._id) })
+    .catch(error => console.log(error));
+
+    document.getElementById('username').value ="";
+    document.getElementById('email').value ="";
+    document.getElementById('phoneNumber').value ="";
 })
 
 
 
-const userlist = document.getElementById('userShow');
+/// function that render the all the user details on the screen 
 
-function showuserOnScreen(obj,userId) {
+function showuserOnScreen(obj, userId) {
     const li = document.createElement('li');
     const dltBtn = document.createElement('button');
     dltBtn.innerText = "delete";
-    dltBtn.setAttribute('id',userId);
+    dltBtn.setAttribute('id', userId);
 
-    dltBtn.addEventListener('click', (event) => {
-        // Call the delete function with the user ID
-        deleteUser(userId);
+    const editBtn = document.createElement("button");
+    editBtn.innerText = 'edit';
+    editBtn.setAttribute('id', userId);
 
-    });
-    
+    // list item created that contains user details
     li.innerHTML = `${obj.username} -- ${obj.email} -- ${obj.phoneNumber}`
     li.append(dltBtn)
-
+    li.append(editBtn);
+    const userlist = document.getElementById('userShow');
     userlist.append(li);
+
+
+    // edit button  event listner
+    editBtn.addEventListener('click', (event) => {
+        editeUser(userId);
+    })
+
+
+    // delete button event listner 
+    dltBtn.addEventListener('click', (event) => {
+        // Call the delete function with the user ID
+        deletRequest(userId)
+    });
+}
+
+
+function editeUser(userId) {
+    getRequest(userId)
+        .then((response) => {
+            document.getElementById('username').value = response.data.username;
+            document.getElementById('email').value = response.data.email;
+            document.getElementById('phoneNumber').value = response.data.phoneNumber;
+        })
+        .catch(error => console.log(error))
+
+    deletRequest(userId)
+
 }
 
 
 
 
-function deleteUser(userId){
-        axios.delete(`https://crudcrud.com/api/3f4210f7265e4b6bb5c86a3348c859b1/BookingAppointment/${userId}`)
+/// Axios requests 
+
+function deletRequest(userId) {
+    axios.delete(`https://crudcrud.com/api/740ceeefa0304d3a9fd3a83e71f39973/BookingAppointment/${userId}`)
         .then(response => {
             const userToRemove = document.getElementById(userId);
             if (userToRemove) {
                 userToRemove.parentNode.remove();
             }
         })
+        .catch(error => console.log(error))
 }
 
 
+function getRequest(userId) {
+    if (userId) {
+        return axios.get(`https://crudcrud.com/api/740ceeefa0304d3a9fd3a83e71f39973/BookingAppointment/${userId}`)
+    }
+    return axios.get("https://crudcrud.com/api/740ceeefa0304d3a9fd3a83e71f39973/BookingAppointment")
+
+}
+
+function postRequest(obj) {
+    return axios.post("https://crudcrud.com/api/740ceeefa0304d3a9fd3a83e71f39973/BookingAppointment", obj)
+}
